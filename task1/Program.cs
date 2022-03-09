752af60fb2dad1f3096c08154864cf04d3baeaca
Fatime668 <tu7dkjdue@code.edu.az>
2022-03-09 12:40:08 +0400

    method

---
+++ task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Max();
+        }
+        public static void Max()
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int max = 0;
+            if (num1 % 7 == 0)
+            {
+                Console.WriteLine("Divide by 7");
+            }
+            else
+            {
+                for (int i = num1; i < num1 * 2; i++)
+                {
+                    if (i % 7 == 0)
+                    {
+                        max = i;
+                        Console.WriteLine(max);
+                        break;
+                    }
+                }
+                return ;
+            }
+        }
+       
+        
+    }
+}
